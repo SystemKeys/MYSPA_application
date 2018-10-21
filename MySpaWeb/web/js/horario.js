82235c152f15c0e2d64d7fe50b6b858881dc4271
dieggh <43835886+dieggh@users.noreply.github.com>
2018-10-21 00:15:46 -0500

    Delete unnesasary web project

--- MySpaWeb/web/js/horario.js
+++
@@ -1,7 +0,0 @@
-var horario=["9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00"];
-function cargarHorario(){
-	var lista=document.getElementById("cmbHorario");
-	for (var i =0; i<= horario.length ; i++) {
-		document.getElementById("cmbHorario").options[i]= new Option(horario[i]);
-	}
-}
