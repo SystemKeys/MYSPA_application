f1904bbfd753621b95c1bed91555fda08d8f0632
dieggh <43835886+dieggh@users.noreply.github.com>
2018-10-26 22:47:56 -0500

    Latest web project;

--- MySPA_CORE/myspa_core/src/org/solsistemas/myspa/controller/ControllerCliente.java
+++ MySPA_CORE/myspa_core/src/org/solsistemas/myspa/controller/ControllerCliente.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -383,5 +383,6 @@ public class ControllerCliente {
         //Devolvemos la lista dinámica con objetos de tipo Empleado dentro:
         return clientes;
     }
-    
+    //jajajajijo
+//ejfowejpofjpoes
 }
