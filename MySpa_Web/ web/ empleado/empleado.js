e5c5c6ba5268d07e92e6033f31a3f7bf727dd3ca
ElDarckar69 <44538393+ElDarckar69@users.noreply.github.com>
2018-11-01 17:48:12 -0600

    Update and rename MySpa_Web/web/empleado/empleado.js to  MySpa_Web/ web/ empleado/empleado.js

--- MySpa_Web/ web/ empleado/empleado.js
+++ MySpa_Web/ web/ empleado/empleado.js
@@ -470,3 +470,169 @@ function limpiarCamposTratamiento() {
     $('#txtDescripcionTratamiento').val('');
     $('#chbEstatusTratamiento').prop('checked', true);
 }
+//Cliente
+function cargarModuloCliente(){
+    $.ajax  (   
+                {   type   :    "GET",
+                    url    :   "cliente/catalogo.html",
+                    async  :    true
+                }
+            ).done(
+                    function(data){
+                      $('#divMainContainer').html(data);  
+                      $('#tbProductos').find('tr').click(function()
+                                                          {
+                                                            alert('Renglon: ' + $(this).index());
+                                                            }                              
+                                                            );
+                        actualizarTablaClientes();
+                        }
+                    );
+                   
+                    
+}
+
+function actualizarTablaClientes(){
+    $.ajax( {
+                                type   : "GET",
+                                url    : "../rscliente/getAllCliente",
+                                async  : true
+                              }).done(
+                                        function(clientes)
+                                            {
+                                                var str  = '';
+                                                for (var i = 0; i < clientes.length; i++)
+                                                    str += '<tr>' + 
+                                                                  '<td>' + clientes[i].id + '</td>' +
+                                                                  '<td>' + clientes[i].persona.nombre +
+                                                                  ' ' + clientes[i].persona.apellidoPaterno +
+                                                                  ' ' + clientes[i].persona.apellidoMaterno + '</td>' +
+                                                                  '<td>' + clientes[i].persona.genero + '</td>' +
+                                                                  '<td>' + clientes[i].persona.domicilio + '</td>' +
+                                                                  '<td>' + clientes[i].persona.telefono + '</td>' +
+                                                                  '<td>' + clientes[i].persona.rfc + '</td>' +
+                                                                  '<td>' + clientes[i].numeroUnico + '</td>' +
+                                                                  '<td>' + clientes[i].correo + '</td>' +
+                                                            '</tr>';
+                                                    $('#tbClientes').html(str);
+                                                    $('#tbClientes').find('tr').click(function()
+                                                          {
+                                                            $('#txtIdCliente').val(clientes[$(this).index()].id);
+                                                            $('#txtIdUsuarioCliente').val(clientes[$(this).index()].usuario.id);
+                                                            $('#txtIdPersonaCliente').val(clientes[$(this).index()].persona.id);
+                                                            $('#txtNumeroUnicoCliente').val(clientes[$(this).index()].numeroUnico);
+                                                            $('#txtNombreCliente').val(clientes[$(this).index()].persona.nombre);
+                                                            $('#txtApellidoPaternoCliente').val(clientes[$(this).index()].persona.apellidoPaterno);
+                                                            $('#txtApellidoMaternoCliente').val(clientes[$(this).index()].persona.apellidoMaterno);
+                                                            $('#txtGeneroCliente').val(clientes[$(this).index()].persona.genero);
+                                                            $('#txtDomicilioCliente').val(clientes[$(this).index()].persona.domicilio);
+                                                            $('#txtTelefonoCliente').val(clientes[$(this).index()].persona.telefono);
+                                                            $('#txtRFCCliente').val(clientes[$(this).index()].persona.rfc);
+                                                            $('#txtCorreoCliente').val(clientes[$(this).index()].correo);
+                                                            $('#txtNombreUsuarioCliente').val(clientes[$(this).index()].usuario.nombreUsuario);
+                                                            $('#txtContraseniaCliente').val(clientes[$(this).index()].usuario.contrasenia);
+                                                            $('#chbEstatusProducto').prop('checked', true);
+                                                            $('#divModalClienteDetalle').modal();
+                                                            }                              
+                                                            );
+                                            }
+                                     );           
+}
+function guardarCliente(){
+    
+    var rutaREST = null;
+    var idCliente = 0;
+    var idPersona = 0;
+    var idUsuario = 0;
+    
+    if($('#txtIdCliente').val().length > 0){
+        rutaREST = '../rscliente/updateCliente';
+        idCliente = $('#txtIdCliente').val();
+        idUsuario = $('#txtIdUsuarioCliente').val();
+        idPersona = $('#txtIdPersonaCliente').val();
+    }else{
+        rutaREST = '../rscliente/insertCliente';
+    }
+    
+    $.ajax({
+            type : "POST",
+            asyn : true,
+            url : rutaREST,
+            data :{
+                    idCliente : idCliente,
+                    idUsuario : idUsuario,
+                    idPersona : idPersona,
+                    nombre : $('#txtNombreCliente').val(),
+                    apellidoPaterno : $('#txtApellidoPaternoCliente').val(),
+                    apellidoMaterno : $('#txtApellidoMaternoCliente').val(),
+                    genero : $('#txtGeneroCliente').val(),
+                    domicilio : $('#txtDomicilioCliente').val(),
+                    telefono : $('#txtTelefonoCliente').val(),
+                    rfc : $('#txtRFCCliente').val(),
+                    correo : $('#txtCorreoCliente').val(),
+                    nombreUsuario : $('#txtNombreUsuarioCliente').val(),
+                    contrasenia : $('#txtContraseniaCliente').val(),
+                    }                        
+            }).done(function (data)
+                    {
+                     if (data.error != null){
+                         swal("Error", data.error, 'warning');
+                         return;
+                     }
+                     actualizarTablaClientes();
+                     $('#txtIdCliente').val(data.id);
+                     $('#txtIdUsuarioCliente').val(data.usuario.id);
+                     $('#txtIdPersonaCliente').val(data.persona.id);
+                     $('#txtNumeroUnicoCliente').val(data.numeroUnico);
+                     swal('Realizado', 'Hecho', 'success');
+                    }                                
+                    );
+}
+
+function eliminarCliente(){
+    
+    var rutaREST = null;
+    
+    if($('#txtIdCliente').val().length > 0){
+        rutaREST = '../rscliente/deleteCliente';
+    }else{
+        swal("Error", "no hay id", 'warning');
+        return;
+    }
+    
+    $.ajax({
+            type : "POST",
+            asyn : true,
+            url : rutaREST,
+            data :{
+                    idCliente : $('#txtIdCliente').val()
+                    }                        
+            }).done(function (data)
+                    {
+                     if (data.error != null){
+                         swal("Error", data.error, 'warning');
+                         return;
+                     }
+                     actualizarTablaClientes();
+                     swal('Realizado', 'Hecho', 'success');
+                    }                                
+                    );
+}
+
+function limpiarCamposCliente(){
+    $('#txtIdCliente').val('');
+    $('#txtNombreCliente').val('');
+    $('#txtApellidoPaternoCliente').val('');
+    $('#txtApellidoMaternoCliente').val('');
+    $('#txtGeneroCliente').val('Genero');
+    $('#txtDomicilioCliente').val('');
+    $('#txtTelefonoCliente').val('');
+    $('#txtRFCCliente').val('');
+    $('#txtCorreoCliente').val('');
+    $('#txtNombreUsuarioCliente').val('');
+    $('#txtContraseniaCliente').val('');
+    $('#txtIdUsuarioCliente').val('');
+    $('#txtIdPersonaCliente').val('');
+    $('#txtNumeroUnicoCliente').val('');
+    $('#chbEstatusProducto').prop('checked', false);
+    }
